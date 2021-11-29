fa1e50355b5cdea148b8cdd53243bde13ed1e875
jonathan <jonathan.henry@epfl.ch>
2021-11-29 16:18:21 +0100

    move v2

--- movement and filter/movement.py
+++ movement and filter/movement.py
@@ -1,26 +1,34 @@
- # %%run_python
+# %%run_python
 import numpy as np
 
 
-def move(pos_prev, pos_goal):
-    wheel_radius =
-    k_rho =
-    k_alpha =
-    k_beta =
+def move(pos_prev, position_goal):
+    wheel_radius = 44       # mm
+    length_robot = 110      # mm
+    k_rho = 20
+    k_alpha = 25
+    k_beta = - 5
     x_prev = pos_prev[1]
     y_prev = pos_prev[2]
-    x_goal = pos_goal[1]
-    y_goal = pos_goal[2]
+    x_goal = position_goal[1]
+    y_goal = position_goal[2]
     theta = pos_prev[3]
     dx = x_goal - x_prev
     dy = y_goal - y_prev
     rho = np.sqrt(dx**2 + dy**2)
     alpha = -theta + np.arctan2(dy, dx)
     beta = -theta - alpha
-    # Rotation_matrix = np.array([[np.cos(theta), np.sin(theta), 0],[-np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
-    rho_dot = -k_rho * rho * np.cos(alpha)
-    alpha_dot = k_rho * np.sin(alpha) - k_alpha * alpha - k_beta * beta
-    beta_dot = -k_rho * np.sin(alpha)
+    v = k_rho * rho
+    w = k_alpha * alpha + k_beta * beta
+    speed_left = (v + w * length_robot)/wheel_radius
+    speed_right = (v - w * length_robot) / wheel_radius
+    return np.array(speed_right, speed_left)
 
-
-
+for i in len(resultat_coor):
+    pos_goal = resultat_coor[i]
+    pos =  # prendre la valeur de Kalman
+    while pos != pos_goal:
+        pos =  # prendre la valeur de Kalman
+        speed = move(pos, pos_goal)
+        motor_right_target = speed[1]
+        motor_left_target = speed[2]
