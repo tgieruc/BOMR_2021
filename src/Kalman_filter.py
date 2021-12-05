d8915788bd6e4dc822a1ce11c3638a23f33b964a
jonathan <jonathan.henry@epfl.ch>
2021-12-05 15:29:56 +0100

    temp kalman to switch computers

--- src/Kalman_filter.py
+++ src/Kalman_filter.py
@@ -13,9 +13,9 @@ class Kalman_filter():
         self.pos_x = vision.robot.center[0][0]
         self.pos_y = vision.robot.center[0][1]
         self.theta = vision.robot.orientation
-        self.rho = np.sqrt(self.pos_x **2 + self.pos_y **2)
+        self.rho = np.sqrt(self.pos_x ** 2 + self.pos_y ** 2)
         self.rho_est = self.rho
-        self.P_est = [1000 * np.ones(2)]
+        self.P_est = 1000 * np.ones(2)
         self.state = "camera_on"
         self.time = 100                                 #"""en ms"""
         self.thymio_to_mm_speed = 0.435                 #"""taken from exercise 8"""
@@ -23,25 +23,26 @@ class Kalman_filter():
         self.qp = 0.04
         self.q_nu = 6.15
         self.Q = np.array([[self.qp, 0], [0, self.q_nu]])
-        self.A = np.array([[1, self.time], [0, 1]])
-
+        self.A = np.array([[1, self.time/1000], [0, 1]])
 
     def compute_pos_cart(self):
         self.pos_x = self.rho_est * np.cos(self.theta)
         self.pos_y = self.rho_est * np.sin(self.theta)
+
     def update_value(self, robot_speed):
         vision.update_robot()
         self.pos_x = vision.robot.center[0][0]
         self.pos_y = vision.robot.center[0][1]
         self.theta = vision.robot.orientation
         if vision.robot_detected:
-            self.state = "camera_not_on"
-        else:
             self.state = "camera_on"
+        else:
+            self.state = "camera_not_on"
         self.rho = np.sqrt(self.pos_x ** 2 + self.pos_y ** 2)
         self.speed = self.thymio_to_mm_speed * (robot_speed[0] + robot_speed[1]) / 2
+
     def update_kalman(self, robot_speed):
-        self.update_value(self, robot_speed)
+        self.update_value(robot_speed)
         """" mise en place du filtre ici """
         rho_est_a_priori = np.dot(self.A, self.rho_est)
         p_est_a_priori = np.dot(self.A, np.dot(self.P_est, self.A.T)) + self.Q
@@ -69,11 +70,5 @@ class Kalman_filter():
         self.rho_est = rho_est_a_priori + np.dot(K, i)
         self.P_est = p_est_a_priori - np.dot(K, np.dot(H, p_est_a_priori))
 
-
-
-
-
-
-
-
-
+kalman = Kalman_filter()
+kalman.update_kalman([0,0])
