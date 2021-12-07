06094ae680c56dfa3054a96ee5360181a7d03850
tgieruc <theo.gieruc@gmail.com>
2021-12-07 16:28:37 +0100

    push 4 gui

--- src/local_navigation.py
+++ src/local_navigation.py
@@ -4,189 +4,168 @@ Created on Tue Dec  7 14:57:23 2021
 
 @author: guill
 """
+from tdmclient import aw
 
-def motors(l_speed=500, r_speed=500, verbose=False):
-    """
-    Sets the motor speeds of the Thymio 
-    param l_speed: left motor speed
-    param r_speed: right motor speed
-    param verbose: whether to print status messages or not
-    """
-    # Printing the speeds if requested
-    if verbose:
-        print("\t\t Setting speed : ", l_speed, r_speed)
-    return {
-        "motor.left.target": [l_speed],
-        "motor.right.target": [r_speed],
+global side
+
+
+def set_speed(speed_left, speed_right, node):
+    v = {
+        "motor.left.target": [speed_left],
+        "motor.right.target": [speed_right],
     }
+    aw(node.set_variables(v))
 
-def test_see_nothing_center(close_threshold=1, verbose=False):
-    """
-    tests if the three middle sensors see nothing
-    """
-    if any([x>close_threshold for x in node['prox.horizontal'][1:-3]]):
-        return False
-    if verbose: print("\t\t degagé de l'obstacle")
-    return True
 
-async def go_straight(motor_speed=100, duration=5, verbose=False):
+def get_speed(node):
+    right_speed = node['motor.right.speed']
+    left_speed = node['motor.left.speed']
+    return [left_speed, right_speed]
+
+
+async def go_straight(motor_speed, duration, node, client):
     """
     goes straight for a certain duration
     """
-    if verbose: print("Starting go straight behaviour")
-        
-    await node.set_variables(motors(motor_speed, motor_speed)) #test with lower speed value
+    set_speed(motor_speed, motor_speed, node)  # test with lower speed value
     await client.sleep(duration)
-    await node.set_variables(motors(0, 0))
-    
-async def NN_avoid(motor_speed=100, sensor_scale=400, verbose=False):
-    global side
+    set_speed(0, 0, node)
+
+
+async def NN_avoid(sensor_scale, node, client, side):
     """
     avoids obstacle with neural network
     """
-    see_nothing = False
-    y = [0,0] 
-    
-    while not see_nothing:
-        #asymetric w_l[1] != w_r[1] so the robot choses one side if it arrives straight on the obstacle
+    y = [0, 0]
+
+    while all([x != 0 for x in node['prox.horizontal'][0:5]]):
+        print("fdp")
+        # asymetric w_l[1] != w_r[1] so the robot choses one side if it arrives straight on the obstacle
         w_l = [5, -10, -5, 4, 0]
         w_r = [-5, -5, 5, 0, 4]
 
-        x = [0,0,0,0,0]
-
-        if verbose: print("Starting avoidance with neural network")
+        x = [0, 0, 0, 0, 0]
 
         prox_horizontal = node['prox.horizontal'][:-2]
         print(prox_horizontal)
-        left_prox = prox_horizontal[0]// sensor_scale
-        right_prox = prox_horizontal[4]// sensor_scale
+        left_prox = prox_horizontal[0] // sensor_scale
+        right_prox = prox_horizontal[4] // sensor_scale
 
         # Memory
-        x[3] = y[0]//10
-        x[4] = y[1]//10        
+        x[3] = y[0] // 10
+        x[4] = y[1] // 10
 
-        for i in range(1,4): #we only take the 3 sensors of the middle
+        for i in range(1, 4):  # we only take the 3 sensors of the middle
             # Get and scale inputs
-            x[i-1] = prox_horizontal[i] // sensor_scale
-            
-        print(x)
-        
-        y = [80,80] 
+            x[i - 1] = prox_horizontal[i] // sensor_scale
 
-        for i in range(len(x)):    
+        print(x)
+
+        y = [80, 80]
+
+        for i in range(len(x)):
             # Compute outputs of neurons and set motor powers
             y[0] = y[0] + x[i] * w_l[i]
             y[1] = y[1] + x[i] * w_r[i]
-        
+
         print(y)
-        
-        if test_see_nothing_center(1,True): 
-            y = [0,0]
-            if left_prox > 1:
-                side = 'left'
-            if right_prox > 1:
-                side = 'right'
-                
-        if test_see_nothing_center(1,True) and left_prox < 1 and right_prox < 1:
-            see_nothing = True    
-                
-            print(side)
-            
-        await node.set_variables(motors(y[0],y[1]))
-        await client.sleep(0.1)#let the variables be updated
-        
-async def turn(motor_speed=100,sensor_scale=400,  verbose=False):
-    global side
+
+        if left_prox > 1:
+            side = 'left'
+        if right_prox > 1:
+            side = 'right'
+
+        await client.sleep(0.1)
+
+        set_speed(y[0], y[1], node)
+        await client.sleep(0.1)  # let the variables be updated
+        return side, 1
+
+
+async def turn(sensor_scale, node, client, side):
     """
     turn until obstcale is detected
     """
     adjust = 3
     doneTurn = False
-    y = [0,0] 
-    
-    
+    y = [0, 0]
+
     if side == 'none':
         print("side not set")
         doneTurn = True
-        
+
     while not doneTurn:
-        
-        if verbose: print("turning",side)
-            
+
         prox_horizontal = node['prox.horizontal'][:-2]
-        left_prox = prox_horizontal[0]// sensor_scale
-        right_prox = prox_horizontal[4]// sensor_scale
-        
+        left_prox = prox_horizontal[0] // sensor_scale
+        right_prox = prox_horizontal[4] // sensor_scale
+
         print(left_prox)
         print(right_prox)
-        
+
         if side == 'right':
             if right_prox <= 1 + adjust:
-                y = [80,0]
+                y = [80, 0]
             if right_prox > 1 + adjust:
                 doneTurn = True
                 print("done turn right")
-                
+
         if side == 'left':
             if left_prox <= 1 + adjust:
-                y = [0,80]
+                y = [0, 80]
             if left_prox > 1 + adjust:
                 doneTurn = True
                 print("done turn left")
-        await node.set_variables(motors(y[0],y[1]))        
-        await client.sleep(0.1)#let the variables be updated
-        
-async def follow_obstacle(motor_speed=100,sensor_scale=400, verbose=False):
-    global side
+        set_speed(y[0], y[1], node)
+        await client.sleep(0.1)  # let the variables be updated
+
+
+async def follow_obstacle(sensor_scale, node, client, side):
     """
     follows the obstacle side until its end
     """
     adjust2 = 1000
     constant_scale = 10
     doneFollow = False
-    y = [0,0] 
-    
+    y = [0, 0]
+
     if side == 'none':
         print("side not set")
         doneFollow = True
-        
+
     while not doneFollow:
-        
-        if verbose: print("following wall detected by",side, 'sensor')
-            
         prox_horizontal = node['prox.horizontal'][:5]
-        left_prox = prox_horizontal[0]// sensor_scale
-        right_prox = prox_horizontal[4]// sensor_scale
-        
-        print(left_prox)
-        print(right_prox)
-        
-        left_prox_adjust = left_prox -adjust2//sensor_scale
-        right_prox_adjust = right_prox -adjust2//sensor_scale
+        left_prox = prox_horizontal[0] // sensor_scale
+        right_prox = prox_horizontal[4] // sensor_scale
+
+        left_prox_adjust = left_prox - adjust2 // sensor_scale
+        right_prox_adjust = right_prox - adjust2 // sensor_scale
         if side == 'right':
-            y = [80,80+right_prox_adjust*constant_scale]
+            y = [80, 80 + right_prox_adjust * constant_scale]
         if side == 'left':
-            y = [80+left_prox_adjust*constant_scale,80]
-            
-        await node.set_variables(motors(y[0],y[1]))    
-        await client.sleep(0.1)#let the variables be updated
-       
+            y = [80 + left_prox_adjust * constant_scale, 80]
+
+        set_speed(y[0], y[1], node)
+        await client.sleep(0.1)  # let the variables be updated
+
         if left_prox < 1 and right_prox < 1:
             doneFollow = True
-            
-async def obstacle_avoidance(verbose=True):
-    done = False
+
+
+async def obstacle_avoidance(node, client):
     side = 'none'
-    while not done:
-        side = 0;
-        await NN_avoid(verbose=verbose)
-        
-        await go_straight(duration =3,verbose=verbose)
-        
-        await turn(verbose=verbose)
-        
-        await follow_obstacle(verbose=verbose)
-        
-        await go_straight(duration=5,verbose=verbose)
-        
-        done = True
+    done = 0
+    while done == 0:
+        side, done = await NN_avoid(400, node, client, side)
+
+    await go_straight(100, 3, node, client)
+    print("went away")
+    await turn(400, node, client, side)
+    print("turned away")
+
+    await follow_obstacle(400, node, client, side)
+    print("followed away")
+
+    await go_straight(100, 5, node, client)
+    print("went away")
+
