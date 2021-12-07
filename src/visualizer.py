b3ac37f3083c1a6ab8e0bbfd9192562a3f916e0f
tgieruc <theo.gieruc@gmail.com>
2021-12-07 19:32:17 +0100

    added bugged kalman

--- src/visualizer.py
+++ src/visualizer.py
@@ -4,7 +4,7 @@ import cv2
 
 def get_visualization_kalman(vision, path_planner, kalman):
     img = get_visualization(vision, path_planner)
-    img = cv2.circle(img, (int(kalman.pos_x[0]), int(kalman.pos_y[0])), 10, (0, 0, 255), -1)
+    img = cv2.circle(img, (int(kalman.rho_est[0,0]), int(kalman.rho_est[1,1])), 10, (0, 0, 255), -1)
     return img
 
 def get_visualization(vision, path_planner):
